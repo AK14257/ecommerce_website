1503943654945da21252d3333188f5216eac7391
Abhishek Kumar Yadav <abhishekyadv347754@gmail.com>
2024-09-10 15:33:18 +0530

    first commit

---
+++ client/src/store/auth-slice/index.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+
+const { createSlice } = require("@reduxjs/toolkit");
+
+const initialState = {
+    isAuthenticated : false,
+    isLoading : false,
+    user : null,
+}
+
+
+
+
+const authSlice = createSlice({
+    name : "auth",
+    initialState,
+    reducers :{
+        setUser:(state,action)=>{
+            
+        }
+    }
+})
